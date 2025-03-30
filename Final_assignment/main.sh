2e266263d6d64bdfee5e00a085fcf5081c8c01b2
stijn <stijnvanhimste@DESKTOP-BSP0VEA>
2025-03-30 11:46:33 +0200

    bigstrided

--- Final_assignment/main.sh
+++ Final_assignment/main.sh
@@ -3,7 +3,7 @@ wandb login
 python3 train.py \
     --data-dir ./data/cityscapes \
     --batch-size 64 \
-    --epochs 40 \
+    --epochs 100 \
     --lr 0.001 \
     --num-workers 10 \
     --seed 42 \
