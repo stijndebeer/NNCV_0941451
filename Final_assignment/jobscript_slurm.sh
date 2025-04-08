db40820a0934182a60885daca35a2cf15236eed3
stijn <stijnvanhimste@DESKTOP-BSP0VEA>
2025-04-08 21:13:49 +0200

    actual_enorm_resnet_coupled_nostem_adj_trainloss

--- Final_assignment/jobscript_slurm.sh
+++ Final_assignment/jobscript_slurm.sh
@@ -4,6 +4,6 @@
 #SBATCH --cpus-per-task=18
 #SBATCH --gpus=1
 #SBATCH --partition=gpu_a100
-#SBATCH --time=06:00:00
+#SBATCH --time=07:00:00
 
 srun apptainer exec --nv --env-file .env container.sif /bin/bash main.sh
