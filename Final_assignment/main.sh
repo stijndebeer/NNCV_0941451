5d50c8b658b6736af1c5ec0c513824ac675ceeb3
stijn <stijnvanhimste@DESKTOP-BSP0VEA>
2025-04-11 17:14:10 +0200

    enorm_dice0013

--- Final_assignment/main.sh
+++ Final_assignment/main.sh
@@ -4,7 +4,7 @@ python3 train.py \
     --data-dir ./data/cityscapes \
     --batch-size 16 \
     --epochs 100 \
-    --lr 0.0007 \
+    --lr 0.0013 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "enorm_dice_0007" \
+    --experiment-id "enorm_dice0013" \
