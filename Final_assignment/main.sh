a86b31fd1961461b8990ba432a0bbd5e91421bfe
stijn <stijnvanhimste@DESKTOP-BSP0VEA>
2025-04-12 20:35:08 +0200

    enormnb_dice_final

--- Final_assignment/main.sh
+++ Final_assignment/main.sh
@@ -2,9 +2,9 @@ wandb login
 
 python3 train.py \
     --data-dir ./data/cityscapes \
-    --batch-size 16 \
+    --batch-size 8 \
     --epochs 50 \
     --lr 0.0007 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "bignb_dice_final" \
+    --experiment-id "enormnb_dice_final" \
