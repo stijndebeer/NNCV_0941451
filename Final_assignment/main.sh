d79d83290f20332f702f5262385bde9e99c664cb
stijn <stijnvanhimste@DESKTOP-BSP0VEA>
2025-04-12 18:45:35 +0200

    enorm_resnet_dice_final

--- Final_assignment/main.sh
+++ Final_assignment/main.sh
@@ -2,9 +2,9 @@ wandb login
 
 python3 train.py \
     --data-dir ./data/cityscapes \
-    --batch-size 16 \
+    --batch-size 8 \
     --epochs 50 \
     --lr 0.0007 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "enorm_dice_final" \
+    --experiment-id "enorm_resnet_dice_final" \
