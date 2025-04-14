72b5f0c6790cfcd0572cc05d31e2d9bd6d75f7f1
stijn <stijnvanhimste@DESKTOP-BSP0VEA>
2025-04-14 14:11:46 +0200

    Unet

--- Final_assignment/main.sh
+++ Final_assignment/main.sh
@@ -3,7 +3,7 @@ wandb login
 python3 train.py \
     --data-dir ./data/cityscapes \
     --batch-size 16 \
-    --epochs 40 \
+    --epochs 100 \
     --lr 0.0007 \
     --num-workers 10 \
     --seed 42 \
