d0e85f8a400e45059b516d6087f70e12ef63f1c9
stijn <stijnvanhimste@DESKTOP-BSP0VEA>
2025-04-15 16:39:23 +0200

    enorm_moretrain256

--- Final_assignment/main.sh
+++ Final_assignment/main.sh
@@ -2,7 +2,7 @@ wandb login
 
 python3 train.py \
     --data-dir ./data/cityscapes \
-    --batch-size 8 \ #2 for 512x512
+    --batch-size 8 \
     --epochs 100 \
     --lr 0.0007 \
     --num-workers 10 \
