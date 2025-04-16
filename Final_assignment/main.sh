aca7d8a91b092b220b3d2dba50b318c9cdd08bbe
stijn <stijnvanhimste@DESKTOP-BSP0VEA>
2025-04-16 10:41:18 +0200

    big_512_nopretrain_nocolorjit

--- Final_assignment/main.sh
+++ Final_assignment/main.sh
@@ -2,7 +2,7 @@ wandb login
 
 python3 train.py \
     --data-dir ./data/cityscapes \
-    --batch-size 8 \
+    --batch-size 2 \
     --epochs 100 \
     --lr 0.0007 \
     --num-workers 10 \
--- Final_assignment/train.py
+++ Final_assignment/train.py
@@ -149,7 +149,7 @@ def get_args_parser():
     parser.add_argument("--lr-min", type=float, default=1e-6, help="Minimum learning rate")
 
     #accumulating gradients
-    parser.add_argument("--accumulation-steps", type=int, default=8, help="Number of steps to accumulate gradients")
+    parser.add_argument("--accumulation-steps", type=int, default=32, help="Number of steps to accumulate gradients")
     return parser                                               #32 for 512x512 8 for 256x256
 
 def dice_score(preds, labels, num_classes, epsilon=1e-6):
