7cb9a718369d0a5fcf9460c81909ebe14fa54e7d
stijn <stijnvanhimste@DESKTOP-BSP0VEA>
2025-04-18 19:57:05 +0200

    final_BIG256_no_ocr

--- Final_assignment/train.py
+++ Final_assignment/train.py
@@ -144,7 +144,7 @@ def get_args_parser():
     parser.add_argument("--lr-min", type=float, default=0.0004, help="Minimum learning rate")
 
     #accumulating gradients
-    parser.add_argument("--accumulation-steps", type=int, default=1, help="Number of steps to accumulate gradients")
+    parser.add_argument("--accumulation-steps", type=int, default=2, help="Number of steps to accumulate gradients")
     return parser
 
 def dice_score(preds, labels, num_classes, epsilon=1e-6):
