6c5d5bfba32374e2d4d1c478d6079b0a7b4ce98b
stijn <stijnvanhimste@DESKTOP-BSP0VEA>
2025-04-18 19:52:09 +0200

    final_HUGE256_no_ocr

--- Final_assignment/main.sh
+++ Final_assignment/main.sh
@@ -2,9 +2,9 @@ wandb login
 
 python3 train.py \
     --data-dir ./data/cityscapes \
-    --batch-size 4 \
+    --batch-size 8 \
     --epochs 100 \
     --lr 0.001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "final_HUGE" \
+    --experiment-id "final_HUGE256_no_ocr" \
